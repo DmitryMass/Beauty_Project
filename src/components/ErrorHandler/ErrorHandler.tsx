f5867003048badbbd37d3bedb0eb9b0960f141ef
Dmitry Moskalenko <dimamikron41@gmail.com>
2023-02-03 13:43:41 +0200

    complete errorHandler on registerStudy

--- src/components/ErrorHandler/ErrorHandler.tsx
+++ src/components/ErrorHandler/ErrorHandler.tsx
@@ -1,4 +1,6 @@
-import { FC, memo, useState } from 'react';
+import { FC, memo } from 'react';
+import closeLogo from '@/assets/icons/closeIcon.svg';
+import { errorHanlder } from '@/styles/errorHandler';
 
 interface IErrorHandlerProps {
   data: any;
@@ -6,25 +8,22 @@ interface IErrorHandlerProps {
 }
 
 const ErrorHandler: FC<IErrorHandlerProps> = ({ data, setResponseData }) => {
-  const [error, setError] = useState<string | null>(data);
-
   const handleClick = () => {
-    setError(null);
     setResponseData(null);
   };
   return (
     <>
-      {error ? (
-        <div
-          onClick={handleClick}
-          className='fixed top-0 left-0 bottom-0 right-0 w-full h-full bg-slate-900 bg-opacity-80 flex justify-center items-center'
-        >
-          <div className='max-w-[400px] bg-slate-600 h-[200px] w-full p-[20px]'>
-            <p>{data}</p>
-            <img src='' alt='' />
-          </div>
+      <div onClick={handleClick} className={errorHanlder.wrapper}>
+        <div className={errorHanlder.container}>
+          <p className='text-gold'>{data}</p>
+          <img
+            className={errorHanlder.closeBtn}
+            src={closeLogo}
+            alt='closeLogo'
+            onClick={handleClick}
+          />
         </div>
-      ) : null}
+      </div>
     </>
   );
 };
---
+++ src/styles/errorHandler.ts
@@ -0,0 +1,7 @@
+export const errorHanlder = {
+  wrapper:
+    'fixed top-0 left-0 bottom-0 right-0 w-full h-full bg-slate-900 bg-opacity-80 flex justify-center items-center',
+  container:
+    'relative flex items-center justify-center max-w-[400px] bg-slate-600 h-[100px] w-full p-[10px]',
+  closeBtn: 'absolute cursor-pointer top-[10px] right-[10px] w-[20px] h-[20px]',
+};
